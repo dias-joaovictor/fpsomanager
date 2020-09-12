f1f97710daf61dfc405dae682fa9f9004893cd86
Joao Dias <joao.dias@temasistemas.com.br>
2020-09-12 16:01:46 -0300

    First commit

---
+++ src/main/java/com/modec/fpsomanager/FpsomanagerApplication.java
@@ -0,0 +1,13 @@
+package com.modec.fpsomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpsomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpsomanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modec/fpsomanager/FpsomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modec.fpsomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpsomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
